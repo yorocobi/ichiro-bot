c3a9adf6e7812a64129d41da6b16b61e64446768
荻巣 裕喜 <info@drafting-group.com>
2013-07-06 03:03:48 +0900

    second commit

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'rubygems'
 require 'sinatra'
-require 'tweet.rb'
+require './tweet.rb'
 
 get '/' do
   'under construction'
 end
 
 get '/random_tweet' do
-  Tweet.new.random_tweet  # 動作チェックが終わったらコメントアウトすること
+  Tweet.new.random_tweet
 end
